69acef9eef475e7447b31ebf76251f22b0da37e6
harshilk <hkosambi@hotmail.com>
2016-04-05 16:13:17 +0530

    first commit

---
+++ app/src/main/java/com/example/mpdemo/demogallery/AppConstant.java
@@ -0,0 +1,20 @@
+package com.example.mpdemo.demogallery;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Avni on 25-01-2016.
+ */
+public class AppConstant {
+    public static final int NUM_OF_COLUMNS = 3;
+
+    // Gridview image padding
+    public static final int GRID_PADDING = 8; // in dp
+
+    // SD card image directory
+    public static final String PHOTO_ALBUM = "gallery";
+
+    // supported file formats
+    public static final List<String> FILE_EXTN = Arrays.asList("jpg", "jpeg", "png");
+}
---
+++ app/src/main/java/com/example/mpdemo/demogallery/adapter/NewActivity.java
@@ -0,0 +1,7 @@
+package com.example.mpdemo.demogallery.adapter;
+
+/**
+ * Created by Avni on 25-01-2016.
+ */
+public class NewActivity {
+}
